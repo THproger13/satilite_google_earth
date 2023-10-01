6aaf286a1e3ed26cb5d02ed4f38d00edfaa58e82
THproger13 <thphysics@naver.com>
2023-10-01 16:33:13 +0900

    기상청 특정지역 화재 주의보 불러오기

--- csv_read.py
+++ csv_read.py
@@ -12,16 +12,18 @@ encoding = 'euc-kr'  # 또는 'cp949' 또는 'euc-kr' 등 올바른 인코딩으
 
 # 파일 열기
 
-df = pd.read_csv('C:\\DATA\\satilite_google_earth\\산림청 국립산림과학원_대형산불위험예보목록정보_20230607 (1).csv', encoding=encoding, error_bad_lines=False)
+df = pd.read_csv('C:\\DATA\\satilite_google_earth\\산림청 국립산림과학원_대형산불위험예보목록정보_20230607 (1).csv', encoding=encoding)
 # df.to_csv('C:\\DATA\\satilite_google_earth\\산림청 국립산림과학원_대형산불위험예보목록정보_20230607.csv', encoding='utf-8', index=False)
-
-# # 시군구명이 '포항시 남구'인 정보 필터링
-# daejeon_info = df[df['시군명'] == '포항시 남구']
-# print(daejeon_info)
 
 # 열 이름 출력
 print(df.columns)
 
-# 처음 몇 행의 데이터 출력
-print(df.head())
+# 시군구명이 '포항시 남구'인 정보 필터링
+daejeon_info = df[df['시도명'].str.contains('대전') ]
+print(daejeon_info)
+
+
+
+# # 처음 몇 행의 데이터 출력
+# print(df.head())
 
